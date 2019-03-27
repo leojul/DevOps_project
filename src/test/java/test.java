40ded3bb060acb5646d0aa7a906b993c79ef7a54
jules <jules.diard@gmail.com>
2019-03-27 11:22:33 +0100

    attention des vrais test

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,9 +1,21 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.*;
 
 
 public class test {
+	int x = 2;
+	int y =3;
 	/*@Test
 	public void trucchose() throws Exception{
 		assertEquals(jeveuxça,enutilisantcettemethode(x,y));
 	}*/
+	@Test
+	public void trucchose() throws Exception{
+		assertEquals(5,x+y);
+	}
+	@Test
+	public void trucchose2() throws Exception{
+		assertEquals(6,x+y);
+	}
 }
