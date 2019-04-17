0cd026db25bf6fc184ff1f1ecd4fbd51c78bdceb
geoliak <geoliak@users.noreply.github.com>
2019-04-17 02:42:34 +0200

    Final version of project

--- src/main/java/Dataframe.java
+++ src/main/java/Dataframe.java
@@ -1,17 +1,21 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedHashMap;
+import java.io.*;
+import java.net.URL;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Dataframe {
     private LinkedHashMap<String, ArrayList> map;
 
+    public Dataframe(){
+        map = new LinkedHashMap<>();
+    }
 
-    public Dataframe(String[] dataArray) {
+    public Dataframe(String[] dataArray) throws Exception {
         map = new LinkedHashMap<>();
         int columnNumber = 0;
         ArrayList<String> names = new ArrayList<>();
 
-        for (int col = 0; col <= columnNumber; col++){
+        for (int col = 0; col <= columnNumber; col++) {
 
             String[] array = dataArray[col].split(",");
             //Delete spaces from column names
@@ -23,39 +27,85 @@ public class Dataframe {
             array[0] = array[0].replaceFirst("\\{", "");
             array[array.length - 1] = array[array.length - 1].replaceFirst("}", "");
 
-//            if (array[0].isEmpty() || array[array.length - 1].isEmpty()) {
-//                throw new IllegalArgumentException("Illegal syntax, column is empty");
-//            }
 
             //make an ArrayList for each named column and count them
-            if(col == 0){
+            if (col == 0) {
                 for (String str : array) {
                     map.put(str, new ArrayList());
-                    names.add(columnNumber,str);
+                    names.add(columnNumber, str);
                     columnNumber++;
                 }
-            }
-            else {
-                //TODO:Add type inference in order to make lists of the correct type
-                ArrayList currentList = map.get(names.get(col- 1));
+            } else {
+                //Create an ArrayList, check the types, and insert into map
+                ArrayList currentList = new ArrayList();
                 if (!Collections.addAll(currentList, array)) {
-                    throw new RuntimeException("Could not add elements to respective column");
+                    throw new Exception("Could not add elements to Arraylist");
                 }
-
+                checkArraylistTypeAndInsert(names.get(col- 1), currentList);
             }
         }
     }
 
-    public ArrayList getColumn(String name){
+    public Dataframe(String filename) throws Exception {
+        map = new LinkedHashMap<>();
+        URL url = getClass().getClassLoader().getResource(filename);
+        File csv = new File(url.getPath());
+        BufferedReader br = new BufferedReader(new FileReader(csv));
+        String line = "";
+        while ((line = br.readLine()) != null) {
+            line.replaceAll(" ", "");
+            String[] col = line.split(",");
+            String label = col[0];
+
+            //We create an arraylist of the elements and check the types before inserting in the map
+            ArrayList al = new ArrayList();
+            if (!Collections.addAll(al, col)) {
+                throw new Exception("Could not add elements to Arraylist");
+            }
+            al.remove(0);
+            checkArraylistTypeAndInsert(label, al);
+
+        }
+    }
+
+    //Checks if the elements are integers or doubles, if an element not in either set is found, throw exception.
+    public void checkArraylistTypeAndInsert(String label, ArrayList al) throws Exception {
+
+        boolean isIntArray = true;
+
+        for (Object element : al) {
+            Scanner scanner = new Scanner((String) element);
+            if (!scanner.hasNextInt()) {
+                isIntArray = false;
+                if (!scanner.hasNextDouble()) {
+                    throw new Exception("Only numbers are accepted in columns, and columns can't be empty");
+                }
+            }
+        }
+        ArrayList listToInsert;
+        if (isIntArray){
+            listToInsert = new ArrayList<Integer>();
+            al.forEach(o -> listToInsert.add(Integer.parseInt((String) o)));
+        }
+        else{
+            listToInsert = new ArrayList<Double>();
+            al.forEach(o -> listToInsert.add(Double.parseDouble((String) o)));
+        }
+        map.put(label, listToInsert);
+    }
+
+    public ArrayList getColumn(String name) {
         return map.get(name);
     }
-    public int getNumberOfColumns(){
+
+    public int getNumberOfColumns() {
         return map.values().size();
     }
+
     //Print dataframe lines from start(inclusive) to end(not inclusive)
-    public void printDataframeLines(int start, int end){
+    public void printDataframeLines(int start, int end) {
         //Check arguments for inconsistencies
-        if (start > end || start < 1){
+        if (start > end || start < 1 || end > getNumberOfLines()+1) {
             throw new IllegalArgumentException("The requested lines cannot be printed, bad argument!");
         }
 
@@ -67,21 +117,125 @@ public class Dataframe {
 
         //Add the last separator and change line
         System.out.println("\t|");
-        for(int i = start; i < end;i++){
+        for (int i = start; i < end; i++) {
             //Print line number
             System.out.print(i + "|");
 
             //Print line elements
-            int line = i-1;
+            int line = i - 1;
             map.forEach((s, arrayList) -> {
                 if (line < arrayList.size()) {
-                    System.out.print("\t" + arrayList.get(line)+"\t|");
-                }
-                else System.out.print("\t\t|");
+                    System.out.print("\t" + arrayList.get(line) + "\t|");
+                } else System.out.print("\t\t|");
             });
             System.out.println();
         }
     }
+
+    //Returns the number of lines in the dataframe, which is effectively the number of elements in its biggest column
+    public int getNumberOfLines(){
+        AtomicInteger max = new AtomicInteger();
+        max.set(0);
+        map.forEach((s, arrayList) -> {
+            if(arrayList.size() > max.get()){
+                max.set(arrayList.size());
+            }
+        });
+        return max.get();
+    }
+
+    public void printFirstLines(int numLines){
+        printDataframeLines(1, numLines+1);
+    }
+
+    public void printLastLines(int numLines){
+        printDataframeLines(getNumberOfLines()-numLines+1,getNumberOfLines()+1);
+    }
+
+    public void printDataframe(){
+        printDataframeLines(1, getNumberOfLines()+1);
+    }
+
+    //Create a new dataframe from the lines of the current one, from start(inclusive) to end(not inclusive)
+    public Dataframe newDataframeFromLines(int start, int end){
+
+        if (start > end || start < 1 || end > getNumberOfLines()+1) {
+            throw new IllegalArgumentException("The requested lines cannot be printed, bad argument!");
+        }
+
+        Dataframe newDataframe = new Dataframe();
+
+        for (Map.Entry<String, ArrayList> entry : map.entrySet()) {
+            ArrayList al = new ArrayList();
+            //Adjustment to have lines start from 1
+            for (int i = start - 1; i < end-1; i++){
+                if (i < entry.getValue().size()) {
+                    al.add(entry.getValue().get(i));
+                }
+            }
+            newDataframe.addColumn(entry.getKey(), al);
+        }
+        return newDataframe;
+    }
+
+    //Create a new dataframe from the columns whose labels are entered in the argument
+    public Dataframe newDataframeFromColumns(String[] names){
+        Dataframe newDataframe = new Dataframe();
+        for (int i = 0; i < names.length; i++) {
+            if(map.containsKey(names[i])){
+                newDataframe.addColumn(names[i],map.get(names[i]));
+            }
+        }
+        return newDataframe;
+    }
+
+    public void addColumn(String label, ArrayList al){
+        map.put(label, al);
+    }
+
+    public double columnMeanValue(String label){
+        ArrayList col = map.get(label);
+        double sum  = 0;
+        for (int i = 0; i < col.size();i++) {
+            sum = sum + Double.valueOf(col.get(i).toString());
+        }
+        return sum/col.size();
+    }
+
+    public double columnMinimumValue(String label) {
+        ArrayList col = map.get(label);
+        double min;
+
+        if (!col.isEmpty()) {
+            min = Double.valueOf(col.get(0).toString());
+
+            for (int i = 0; i < col.size(); i++) {
+                if (min > Double.valueOf(col.get(i).toString())) {
+                    min = Double.valueOf(col.get(i).toString());
+                }
+            }
+        }
+        else throw new RuntimeException("The column is empty");
+        return min;
+    }
+
+    public double columnMaximumValue(String label) {
+        ArrayList col = map.get(label);
+        double max;
+
+        if (!col.isEmpty()) {
+            max = Double.valueOf(col.get(0).toString());
+
+            for (int i = 0; i < col.size(); i++) {
+                if (max < Double.valueOf(col.get(i).toString())) {
+                    max = Double.valueOf(col.get(i).toString());
+                }
+            }
+        }
+        else throw new RuntimeException("The column is empty");
+        return max;
+    }
+
 
 
 
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -6,9 +6,15 @@ import java.io.PrintStream;
 
 public class test {
 
-    //Simple constructor test
     @Test
     public void dataframe_const1_test1() throws Exception {
+        Dataframe d = new Dataframe();
+        assert (d.getNumberOfColumns() == 0);
+    }
+
+    //Simple constructor test
+    @Test
+    public void dataframe_const2_test1() throws Exception {
         String[] str = {"{foo,bar}", "{1,2}", "{3,4}"};
         Dataframe d = new Dataframe(str);
         assertEquals("[1, 2]",d.getColumn("foo").toString());
@@ -18,31 +24,182 @@ public class test {
 
     //Simple constructor test with spaces that should be stripped, and empty column
     @Test
-    public void dataframe_const1_test2() throws Exception {
-        String[] str = {"{ foo, bar,goo }", "{1, 2 ,3}", "{4,5,6,7 }", "{ }"};
+    public void dataframe_const2_test2() throws Exception {
+        String[] str = {"{ foo, bar,goo }", "{1, 2 ,3}", "{4,5,6,7 }", "{4, 8 ,9 }"};
         Dataframe d = new Dataframe(str);
         assertEquals("[1, 2, 3]",d.getColumn("foo").toString());
         assertEquals("[4, 5, 6, 7]",d.getColumn("bar").toString());
-        assertEquals("[]",d.getColumn("goo").toString());
+        assertEquals("[4, 8, 9]",d.getColumn("goo").toString());
         assertEquals(3, d.getNumberOfColumns());
     }
 
     @Test
-    public void dataframe_print_test1(){
-        String[] str = {"{ foo, bar,goo }", "{1, 2 ,3}", "{4,5,6,7 }", "{ }"};
+    public void dataframe_const2_test3() throws Exception {
+        String[] str = {"{ foo, bar, goo }", "{1, 2 ,3}", "{string,5,7 }", "{4, 8 ,9 }"};
+        try {
+            Dataframe d = new Dataframe(str);
+            fail("Strings should not be accepted in dataframes");
+        } catch (Exception e) {
+            assert (e.getMessage().contains("Only numbers are accepted"));
+        }
+    }
+
+    @Test
+    public void dataframe_print_test1() throws Exception {
+        String[] str = {"{ foo, bar }", "{1, 2 ,3}", "{4,5,6,7 }"};
         Dataframe d = new Dataframe(str);
         final PrintStream sysOut = System.out;
         final ByteArrayOutputStream testOut = new ByteArrayOutputStream();
         System.setOut(new PrintStream(testOut));
-        d.printDataframeLines(1,5);
-        assertEquals(" |\tfoo |\tbar |\tgoo\t|\n" +
-                "1|\t1\t|\t4\t|\t\t|\n" +
-                "2|\t2\t|\t5\t|\t\t|\n" +
-                "3|\t3\t|\t6\t|\t\t|\n" +
-                "4|\t\t|\t7\t|\t\t|\n",
-                testOut.toString());
+        d.printDataframeLines(1, 5);
+        assertEquals(
+                " |\tfoo |\tbar\t|\n" +
+                        "1|\t1\t|\t4\t|\n" +
+                        "2|\t2\t|\t5\t|\n" +
+                        "3|\t3\t|\t6\t|\n" +
+                        "4|\t\t|\t7\t|\n",testOut.toString());
+        System.setOut(sysOut);
+    }
+    @Test
+    public void dataframe_print_test2() throws Exception {
+        String[] str = {"{ foo, bar }", "{1, 2 ,3}", "{4,5,6,7 }"};
+        Dataframe d = new Dataframe(str);
+        final PrintStream sysOut = System.out;
+        final ByteArrayOutputStream testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+        try {
+            d.printDataframeLines(10, 5);
+            fail("This should not run, lines should be given start to end");
+        } catch (Exception e) {
+            assert(e.getMessage().contains("bad argument"));
+        }
+    }
+
+    @Test
+    public void dataframe_doubles_test1() throws Exception {
+        String[] str = {"{ column1, col2 , column_3 }", "{1.555, 2.2 ,3}", "{45, 700.5 }", "{100, 80.1 }"};
+        Dataframe d = new Dataframe(str);
+        assertEquals("[1.555, 2.2, 3.0]",d.getColumn("column1").toString());
+        assertEquals("[45.0, 700.5]",d.getColumn("col2").toString());
+        assertEquals("[100.0, 80.1]",d.getColumn("column_3").toString());
+    }
+
+    @Test
+    public void dataframe_doubles_test2() throws Exception {
+        String[] str = {"{ column1, col2 , column_3 }", "{1.555, 2.2 ,3}", "{0.1, .5 , -2.5  }", "{100, 80.1 }"};
+        Dataframe d = new Dataframe(str);
+
+        assertEquals("[1.555, 2.2, 3.0]",d.getColumn("column1").toString());
+        assertEquals("[0.1, 0.5, -2.5]",d.getColumn("col2").toString());
+        assertEquals("[100.0, 80.1]",d.getColumn("column_3").toString());
+    }
+
+    @Test
+    public void dataframe_print_first_lines_test() throws Exception {
+        String[] str = {"{ column1, col2 , column_3 }", "{1.555, 2.2 ,3}", "{45, 700.5 }", "{100, 80.1 }"};
+        Dataframe d = new Dataframe(str);
+        final PrintStream sysOut = System.out;
+        final ByteArrayOutputStream testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+        d.printFirstLines(2);
+        assertEquals(
+                " |\tcolumn1 |\tcol2 |\tcolumn_3\t|\n" +
+                        "1|\t1.555\t|\t45.0\t|\t100.0\t|\n" +
+                        "2|\t2.2\t|\t700.5\t|\t80.1\t|\n",testOut.toString());
         System.setOut(sysOut);
     }
 
+    @Test
+    public void dataframe_print_last_lines_test() throws Exception {
+        String[] str = {"{ col1, col2 , col3 }", "{1.555, 2.2 ,3,55}", "{45, 700.5, .2 }", "{100, 80.1 }"};
+        Dataframe d = new Dataframe(str);
+        final PrintStream sysOut = System.out;
+        final ByteArrayOutputStream testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+        d.printLastLines(2);
+        assertEquals(
+                " |\tcol1 |\tcol2 |\tcol3\t|\n" +
+                        "3|\t3.0\t|\t0.2\t|\t\t|\n" +
+                        "4|\t55.0\t|\t\t|\t\t|\n",testOut.toString());
+        System.setOut(sysOut);
+    }
+    @Test
+    public void dataframe_print_dataframe_test() throws Exception {
+        String[] str = {"{ col1, col2 , col3 }", "{1.555, 2.2 ,3,55}", "{45, 700.5, .2 }", "{100, 80.1 }"};
+        Dataframe d = new Dataframe(str);
+        final PrintStream sysOut = System.out;
+        final ByteArrayOutputStream testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+        d.printDataframe();
+        assertEquals(
+                " |\tcol1 |\tcol2 |\tcol3\t|\n" +
+                        "1|\t1.555\t|\t45.0\t|\t100.0\t|\n" +
+                        "2|\t2.2\t|\t700.5\t|\t80.1\t|\n" +
+                        "3|\t3.0\t|\t0.2\t|\t\t|\n" +
+                        "4|\t55.0\t|\t\t|\t\t|\n",testOut.toString());
+        System.setOut(sysOut);
+    }
 
+    @Test
+    public void dataframe_column_min_test() throws Exception {
+        String[] str = {"{ col1, col2 , col3 }", "{1.555, 2.2 ,3,55}", "{45, 700.5, .2 }", "{100, 80 }"};
+        Dataframe d = new Dataframe(str);
+        assertEquals(1.555 , d.columnMinimumValue("col1"),0);
+        assertEquals(0.2 , d.columnMinimumValue("col2"),0);
+        assertEquals(80 , d.columnMinimumValue("col3"),0);
+    }
+
+    @Test
+    public void dataframe_column_max_test() throws Exception {
+        String[] str = {"{ col1, col2 , col3 }", "{1.555, 2.2 ,3,55}", "{45, 700.5, .2 }", "{100, 80 }"};
+        Dataframe d = new Dataframe(str);
+        assertEquals(55 , d.columnMaximumValue("col1"),0);
+        assertEquals(700.5 , d.columnMaximumValue("col2"),0);
+        assertEquals(100 , d.columnMaximumValue("col3"),0);
+
+    }
+
+    @Test
+    public void dataframe_create_from_columns_test() throws Exception {
+        String[] str = {"{ col1, col2 , col3 }", "{1.555, 2.2 ,3,55}", "{45, 700.5, .2 }", "{100, 80.1 }"};
+        Dataframe d = new Dataframe(str);
+        String[] columns = {"col1"};
+        Dataframe nd = d.newDataframeFromColumns(columns);
+        assertEquals(nd.getNumberOfColumns() ,1);
+        assertEquals(1.555 , nd.columnMinimumValue("col1"),0);
+        assertEquals(55 , nd.columnMaximumValue("col1"),0);
+    }
+
+    @Test
+    public void dataframe_create_from_lines_test() throws Exception {
+        String[] str = {"{ col1, col2 , col3 }", "{1.555, 2.2 ,3,55}", "{45, 700.5, .2 }", "{100, 80.1 }"};
+        Dataframe d = new Dataframe(str);
+        String[] columns = {"col1"};
+        Dataframe nd = d.newDataframeFromLines(2, 3);
+        assertEquals (nd.getNumberOfColumns() , 3);
+        assertEquals (2.2 , nd.columnMinimumValue("col1"),0);
+        assertEquals (2.2 , nd.columnMaximumValue("col1"),0);
+        assertEquals (700.5 , nd.columnMinimumValue("col2"),0);
+        assertEquals (80.1, nd.columnMaximumValue("col3"),0);
+        assertEquals (80.1, nd.columnMinimumValue("col3"),0);
+    }
+
+    @Test
+    public void dataframe_calculate_mean_test() throws Exception {
+        String[] str = {"{ col1, col2 , col3 }", "{1.555, 2.2 ,3,55}", "{100, 700 }", "{100, 80.1 }"};
+        Dataframe d = new Dataframe(str);
+        assertEquals(15.43875, d.columnMeanValue("col1"),0);
+        assertEquals(400, d.columnMeanValue("col2"),0);
+        assertEquals(90.05, d.columnMeanValue("col3"),0);
+    }
+
+    @Test
+    public void dataframe_const3_test1() throws Exception {
+        Dataframe d = new Dataframe("Dataframe.csv");
+        assertEquals(4,d.getNumberOfColumns());
+        assertEquals(4 , d.columnMaximumValue("col1"),0);
+        assertEquals(2.8, d.columnMinimumValue("col2"),0);
+        assertEquals(185, d.columnMeanValue("col3"),0);
+        assertEquals(10,d.columnMinimumValue("col4"),0);
+    }
 }
